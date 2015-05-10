feccb04f9ffcbf8afa744a8833f01db96060f0e1
sweaver2494 <sweaver2494@gmail.com>
2015-05-10 14:58:07 -0500

    Picture path reorg and Create CSV Python Updated

--- opencv/sources/modules/contrib/doc/facerec/src/create_csv.py
+++ opencv/sources/modules/contrib/doc/facerec/src/create_csv.py
@@ -26,18 +26,13 @@ import os.path
 
 if __name__ == "__main__":
 
-    if len(sys.argv) != 2:
-        print "usage: create_csv <base_path>"
-        sys.exit(1)
 
-    BASE_PATH=sys.argv[1]
+    BASE_PATH="C:\\Users\\Scott\\Documents\\GitHub\\Face-Recognition\\photos\\cropped\\"
     SEPARATOR=";"
 
     label = 0
     for dirname, dirnames, filenames in os.walk(BASE_PATH):
-        for subdirname in dirnames:
-            subject_path = os.path.join(dirname, subdirname)
-            for filename in os.listdir(subject_path):
-                abs_path = "%s/%s" % (subject_path, filename)
-                print "%s%s%d" % (abs_path, SEPARATOR, label)
-            label = label + 1
+        for filename in os.listdir(subject_path):
+            abs_path = "%s/%s" % (subject_path, filename)
+            print "%s%s%d" % (abs_path, SEPARATOR, label)
+        label = label + 1
